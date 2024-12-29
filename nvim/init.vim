2ab53c747feb758ded17965e3e03cca8016795b9
Ghazi Akel <ghaziaakel@gmail.com>
2024-12-29 11:27:49 -0500

    one month later

--- nvim/init.vim
+++ nvim/init.vim
@@ -4,27 +4,22 @@ filetype plugin indent on
 
 set expandtab ts=4 sw=4
 set ignorecase incsearch
-set noswapfile
 set nu rnu
+set linebreak
+set noswapfile
 set scrolloff=5 mouse=a
 set wildmenu wildoptions=pum
 
 call plug#begin()
-    Plug 'lambdalisue/fern.vim'
     Plug 'jiangmiao/auto-pairs'
     Plug 'tpope/vim-commentary'
     Plug 'lervag/vimtex'
-    Plug 'nvim-lua/plenary.nvim'
-    Plug 'nvim-telescope/telescope.nvim'
-    Plug 'nvim-treesitter/nvim-treesitter'
 call plug#end()
 
 let g:vimtex_view_automatic = 0
 
 set termguicolors
 colorscheme habamax
-highlight Normal ctermbg=NONE guibg=NONE
-let g:netrw_banner=0
 
 " Find files using Telescope command-line sugar.
 nnoremap <leader>ff <cmd>Telescope find_files<cr>
@@ -37,5 +32,6 @@ nnoremap <C-u> <C-u>zz
 nnoremap <tab> :bn <CR>
 nnoremap <s-tab> :bp <CR>
 nnoremap <C-n> :set nonu! nornu! <CR>
+nnoremap <C-tab> :setlocal formatoptions+=a <CR>
 autocmd InsertEnter,WinLeave * if &number | set nornu | endif
 autocmd InsertLeave,WinEnter * if &number | set rnu | endif
--- scripts/load_workspaces.sh
+++ scripts/load_workspaces.sh
@@ -1,120 +1,89 @@
 #!/bin/bash
 
-# i3-save-tree --workspace "x" > workspace_x.json
+get_current_workspace() {
+    i3-msg -t get_workspaces | jq '.[] | select(.focused == true).num'
+}
 
-# Function to load workspace 0
-load_workspace_0() {
-    i3-msg '[workspace="0"] kill'
-    i3-msg '[workspace="0: config"] kill'
-    sleep 0.1
-    i3-msg 'workspace "0"'
-    i3-msg 'append_layout ~/.config/i3workspaces/workspace_0.json'
-
+load_preset_0() {
     (firefox https://github.com/ghazinator/config \
-        https://wiki.archlinux.org/title/Main_page &)
+            https://archlinux.org/ &)
+
     (alacritty -e bash -c "nvim ~/.config/i3/config \
-               ~/.config/scripts/load_workspaces.sh \
-                            ~/.config/nvim/init.vim \
-                            ~/.config/i3blocks/config; $SHELL" &)
+                ~/.config/scripts/load_workspaces.sh \
+                ~/.config/nvim/init.vim \
+                ~/.config/i3blocks/config; $SHELL" &)
     sleep 0.1
     (alacritty -e bash -c "htop; $SHELL" &)
-    sleep 0.1
-    i3-msg 'rename workspace 0 to "0: config"'
 }
 
-# Function to load workspace 1
-load_workspace_1() {
-    i3-msg '[workspace="1"] kill'
-    i3-msg '[workspace="1: discord"] kill'
-    sleep 0.1
-    i3-msg 'workspace "1"'
-    i3-msg 'append_layout ~/.config/i3workspaces/workspace_1.json'
-
+load_preset_1() {
     (discord &)
-    sleep 0.1
-    i3-msg 'rename workspace 1 to "1: discord"'
 }
 
-load_workspace_2() {
-    i3-msg '[workspace="2"] kill'
-    i3-msg '[workspace="2: firefox"] kill'
+load_preset_2() {
+    (firefox https://www.youtube.com/ https://destiny.gg &)
     sleep 0.1
-    i3-msg 'workspace "2"'
-    i3-msg 'append_layout ~/.config/i3workspaces/workspace_2.json'
-
-    (firefox https://destiny.gg https://www.youtube.com/ \
-    https://www.reddit.com/ https://bsky.app/ &)
-    sleep 0.1
-    i3-msg 'rename workspace 2 to "2: firefox"'
+    (firefox https://www.reddit.com/ https://bsky.app/ &)
 }
 
-load_workspace_3() {
-    i3-msg '[workspace="3"] kill'
-    i3-msg '[workspace="3: resume"] kill'
-    sleep 0.1
-    i3-msg 'workspace "3"'
-    i3-msg 'append_layout ~/.config/i3workspaces/workspace_3.json'
-
-    (firefox https://mail.google.com/ https://gemini.google.com/app &)
+load_preset_3() {
+    (firefox https://mail.google.com/ https://www.linkedin.com/feed/ \
+            https://chat.com &)
     sleep 0.1
 
-    (alacritty -e bash -c "zathura Documents/Resume/resume.pdf & \
-        nvim Documents/Resume/*.tex; $SHELL" &)
+    (zathura Documents/Resume/coverletter.pdf &)
+    (alacritty -e nvim -c "VimtexCompile" Documents/Resume/coverletter.tex &)
     sleep 0.1
-    i3-msg 'rename workspace 3 to "3: resume"'
+    (zathura Documents/Resume/resume.pdf &)
+    (alacritty -e nvim -c "VimtexCompile" Documents/Resume/resume.tex \
+                Documents/Resume/resumebody.tex &)
 }
 
-load_workspace_7() {
-    i3-msg '[workspace="7"] kill'
-    i3-msg '[workspace="7: steam"] kill'
-    sleep 0.1
-    i3-msg 'workspace "7"'
-    i3-msg 'append_layout ~f.config/i3workspaces/workspace_7.json'
-
+load_preset_8() {
     (steam &)
-    sleep 0.1
-    i3-msg 'rename workspace 7 to "7: steam"'
 }
 
-load_workspace_8() {
-    i3-msg '[workspace="8"] kill'
-    i3-msg '[workspace="8: vlc"] kill'
-    sleep 0.1
-    i3-msg 'workspace "8"'
-    i3-msg 'append_layout ~/.config/i3workspaces/workspace_7.json'
-
-    (vlc &)
-    sleep 0.1
-    i3-msg 'rename workspace 8 to "8: vlc"'
-}
-
-load_workspace_9() {
-    i3-msg '[workspace="9"] kill'
-    i3-msg '[workspace="9: control"] kill'
-    sleep 0.1
-    i3-msg 'workspace "9"'
-    i3-msg 'append_layout ~/.config/i3workspaces/workspace_9.json'
-
+load_preset_9() {
     (helvum &)
     sleep 0.1
     (GTK_THEME=Adwaita:dark pavucontrol &)
-    sleep 0.1
-    i3-msg 'rename workspace 9 to "9: control"'
 }
 
-# Get workspace number from i3-input
-workspace_num=$(i3-input -P 'Enter workspace number: ' -F '%s' | tail -n 1)
-workspace_num="${workspace_num: -1}"  # Extract the last character
+# Toggle this variable to switch between modes
+use_original_functionality=true  
 
-# Case switch to load the selected workspace
-case "$workspace_num" in
-    0) load_workspace_0 ;;
-    1) load_workspace_1 ;;
-    2) load_workspace_2 ;;
-    3) load_workspace_3 ;;
-    7) load_workspace_7 ;;
-    8) load_workspace_8 ;;
-    9) load_workspace_9 ;;
-    *) echo "Invalid workspace number" ;;
-esac
+preset_num=$(i3-input -P 'Enter preset number: ' -F '%s' | tail -n 1)
+preset_num="${preset_num: -1}" # Extract the last character
 
+current_workspace=$(get_current_workspace)
+
+if $use_original_functionality; then
+    case "$preset_num" in
+        [0-9])
+            i3-msg -t command "[workspace=\"$preset_num\"] kill" 
+            sleep 0.1
+            i3-msg -t command "workspace \"$preset_num\""
+            i3-msg -t command 'append_layout ~/.config/i3workspaces/workspace_'${preset_num}'.json'
+            "load_preset_$preset_num"
+            ;;
+        *) echo "Invalid preset number" ;;
+    esac
+else
+    case "$preset_num" in
+        0) 
+            i3-msg -t command '[workspace="0"] kill'
+            sleep 0.1
+            i3-msg -t command 'workspace "0"'
+            i3-msg -t command 'append_layout ~/.config/i3workspaces/workspace_0.json'
+            load_preset_0
+            ;;
+        [1-9])
+            i3-msg -t command "[workspace=\"$current_workspace\"] kill" 
+            sleep 0.1
+            i3-msg -t command "workspace \"$current_workspace\""
+            i3-msg -t command 'append_layout ~/.config/i3workspaces/workspace_'${preset_num}'.json'
+            "load_preset_$preset_num"
+            ;;
+        *) echo "Invalid preset number" ;;
+    esac
+fi
