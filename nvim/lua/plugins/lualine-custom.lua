451608d4f56fec603bdd834a12123f98abd4558a
Ghazi Akel <ghaziaakel@gmail.com>
2026-01-16 18:32:02 -0500

    cleaned up file

--- nvim/lua/plugins/lualine-custom.lua
+++ nvim/lua/plugins/lualine-custom.lua
@@ -3,13 +3,9 @@ return {
 
   ---@param opts table
   opts = function(_, opts)
-    -- Task 1: Remove Powerline separators (This part is correct)
     opts.options.component_separators = { left = '', right = '' }
     opts.options.section_separators = { left = '', right = '' }
 
-    -- Task 2: Remove the time
-    -- Set the 'lualine_z' section to be an empty table.
-    -- This removes the time but doesn't add a new 'location'.
-    opts.sections.lualine_z = {} -- <-- THIS IS THE FIX
+    opts.sections.lualine_z = {}
   end
 }
